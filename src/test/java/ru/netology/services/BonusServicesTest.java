b60624f53e5a74afbca05a5450a3a58c2f69cf50
Maksim Aptishev GitHub <maximus.testqa777@gmail.com>
2023-04-20 19:59:46 +0300

    add csv file

--- src/test/java/ru/netology/services/BonusServicesTest.java
+++ src/test/java/ru/netology/services/BonusServicesTest.java
@@ -1,32 +1,39 @@
 package ru.netology.services;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
-
-import javax.annotation.processing.SupportedAnnotationTypes;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class BonusServicesTest {
 
-    @Test
-    public void BonusServiceCalculateVacationMonths3() {
+    @ParameterizedTest
+   /* @CsvSource({
+            "3,10000,3000,20000",
+            "2,100000,60000,150000"
+    })*/
+    @CsvFileSource(files = "src/test/resources/bonus.csv")
+    public void BonusServiceCalculateVacationMonths(int expected, int income, int expense, int threshold) {
 
         var services = new BonusServices();
 
         // подготавливвем данные:
-        int income = 10000;
-        int expense = 3000;
-        int threshold = 20000;
+        //int income = 10000;
+        //int expense = 3000;
+        //int threshold = 20000;
 
 
         // вызываем целевой метод
         int actual = services.calculate(income, expense, threshold);
-        int expected = 3;
+
+
 
         // произодим проверку (ожидаемый, фактический)
         Assertions.assertEquals(expected, actual);
+
     }
 
-    @Test
+    /*@Test
     public void BonusServiceCalculateVacationMonths2() {
 
         var services = new BonusServices();
@@ -42,5 +49,5 @@ public class BonusServicesTest {
 
         // произодим проверку (ожидаемый, фактический)
         Assertions.assertEquals(expected, actual);
-    }
+    }*/
 }
